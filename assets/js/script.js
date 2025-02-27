c2c154cf49c71c67fdb542eca16bd3e4debe9979
website-template <khiljiasad2@gmail.com>
2025-02-27 01:04:12 -0500

    added

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,27 +32,21 @@ const createNav = () => {
 
     nav.innerHTML = `
         <nav>
+            <a href="index.html">
             <div class="dropdown">
                 <span>Home</span>
             </div>
+            </a>
+            <a href="#about-us">
             <div class="dropdown">
                 <span>About Us</span>
             </div>
+            </a>
+            <a href="#our-services">
             <div class="dropdown">
                 <span>Services</span>
-                <div class="dropdown-content">
-                  <a href="#"><p>Basement Remodeling</p></a>
-                  <a href="#"><p>Bathroom Remodeling</p></a>
-                  <a href="#"><p>Deck & Patio</p></a>
-                  <a href="#"><p>Exterior Renovation</p></a>
-                  <a href="#"><p>Home Addition</p></a>
-                  <a href="#"><p>Home Remodeling</p></a>
-                  <a href="#"><p>Interior Renovation</p></a>
-                  <a href="#"><p>Kitchen Remodeling</p></a>
-                  <a href="#"><p>Cad Service</p></a>
-                  <a href="#"><p>Our Prices Roadmap</p></a>
-                </div>
             </div>
+            </a>
             <div class="dropdown">
                 <span>Areas</span>
                 <div class="dropdown-content">
@@ -62,18 +56,26 @@ const createNav = () => {
                   <a href="#"><p>Chester, NJ</p></a>
                 </div>
             </div>
+            <a href="#projects">
             <div class="dropdown">
                 <span>Projects</span>
             </div>
+            </a>
+            <a href="#reviews">
             <div class="dropdown">
                 <span>Reviews</span>
             </div>
+            </a>
+            <a href="#contact-us">
             <div class="dropdown">
                 <span>Contact Us</span>
             </div>
+            </a>
+            <a href="#contact-us">
              <div class="dropdown">
                 <span>Free Estimates</span>
             </div>
+            </a>
         </nav>
     `;
 }
@@ -90,23 +92,24 @@ const createFooter = () => {
             </div>
             <div class="col-4">
                 <h3>QUICK LINKS</h3>
-                <a href="#"><p>Home</p></a>
-                <a href="#"><p>About Us</p></a>
-                <a href="#"><p>Reviews</p></a>
-                <a href="#"><p>Contact</p></a>
+                <a href="index.html"><p>Home</p></a>
+                <a href="#about-us"><p>About Us</p></a>
+                <a href="#reviews"><p>Reviews</p></a>
+                <a href="#contact-us"><p>Contact</p></a>
             </div>
+            <a href="#our-services">
             <div class="col-4">
                 <h3>OUR SERVICES</h3>
-                <a href="#"><p>Basement Remodeling</p></a>
-                <a href="#"><p>Bathroom Remodeling</p></a>
-                <a href="#"><p>Home Remodeling</p></a>
-                <a href="#"><p>Home Addition</p></a>
+                <a href="#our-services"><p>Basement Remodeling</p></a>
+                <a href="#our-services"><p>Bathroom Remodeling</p></a>
+                <a href="#our-services"><p>Kitchen Remodeling</p></a>
             </div>
+            </a>
             <div class="col-4">
                 <h3>CONTACT</h3>
-                <a href="#"><p>📞 862.223.0358</p></a>
-                <a href="#"><p>✉️ somnium_home@icloud.com</p></a>
-                <a href="#"><p>📍 Roxbury Township, NJ</p></a>
+                <a href="tel:862-223-0358"><p>📞 862.223.0358</p></a>
+                <a href="mailto:somnium_home@icloud.com"><p>✉️ somnium_home@icloud.com</p></a>
+                <p>📍 Roxbury Township, NJ</p>
             </div>
         </div>
     `;
